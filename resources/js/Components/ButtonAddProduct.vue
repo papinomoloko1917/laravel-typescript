c81e8341ab5b53fff67a5b63489b9d8bd21710c0
papinomoloko1917 <varivodamax88@gmail.com>
2026-03-17 22:00:20 +0300

    update

---
+++ resources/js/Components/ButtonAddProduct.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+
+const show = ref(false);
+const added = ref(false);
+const count = ref(1);
+
+function upCount() {
+    ++count.value;
+}
+
+function downCount() {
+    if (count.value > 1) --count.value;
+}
+
+const addedProduct = computed(() => {
+    return !added.value ? 'Добавить' : 'Добавлено <svg xmlns="http://www.w3.org/2000/svg" width = "24" height = "24" viewBox = "0 0 24 24" fill = "none" stroke = "currentColor" stroke - width="2" stroke - linecap="round" stroke - linejoin="round" class="lucide lucide-check-icon lucide-check" > <path d="M20 6 9 17l-5-5" /> </svg>';
+});
+
+</script>
+
+<template>
+    <button @click="show = true" v-if="!show" class="btn btn-primary">В корзину</button>
+    <div v-else class="flex">
+        <div v-if="!added" class="flex gap-0.5">
+            <button @click="upCount()" class="btn btn-primary">+</button>
+            <button class="btn btn-primary cursor-default">{{ count }}</button>
+            <button @click="downCount()" class="btn btn-primary">-</button>
+        </div>
+        <button @click="added = true" :class="!added ? 'btn btn-primary ml-3' : 'text-white btn btn-success ml-3'"
+            v-html="addedProduct"></button>
+    </div>
+</template>
--- resources/js/Components/ProductCard.vue
+++ resources/js/Components/ProductCard.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { Product } from '../types/product';
+import ButtonAddProduct from './ButtonAddProduct.vue';
 
 const { product } = defineProps<{
     product: Product
@@ -8,15 +9,16 @@ const { product } = defineProps<{
 
 <template>
 
-    <div class="card bg-base-100 w-96 shadow-sm">
-        <figure class="h-56 w-full overflow-hidden bg-base-200">
-            <img :src="`/${product.image}`" :alt="product.title" class="w-full h-full object-contain" />
+    <div class="mt-7 card bg-base-100 w-96 shadow-sm">
+        <figure class="h-56 w-full overflow-hidden bg-base-100">
+            <img :src="`/${product.image}`" :alt="product.title"
+                class="w-full h-full object-contain hover:scale-103 transition-all duration-300" />
         </figure>
         <div class="card-body">
             <h2 class="card-title">{{ product.title }}</h2>
             <p>{{ product.description }}</p>
             <div class="card-actions justify-end">
-                <button class="btn btn-primary">В корзину</button>
+                <ButtonAddProduct />
             </div>
         </div>
     </div>
