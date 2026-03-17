54dba0a4d4682f0ef598a958323b42a2daae57ea
ninjamax1917 <ninjamax177@gmail.com>
2026-03-18 00:37:05 +0300

    update

--- resources/js/Components/ButtonAddProduct.vue
+++ resources/js/Components/ButtonAddProduct.vue
@@ -1,33 +1,67 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { computed, onBeforeUnmount, ref } from 'vue';
 
-const show = ref(false);
-const added = ref(false);
+const isControlsVisible = ref(false);
+const isAdded = ref(false);
 const count = ref(1);
+let timer: ReturnType<typeof setTimeout> | null = null;
 
-function upCount() {
-    ++count.value;
+function upCount(): void {
+  count.value++;
 }
 
-function downCount() {
-    if (count.value > 1) --count.value;
+function downCount(): void {
+  if (count.value > 1) count.value--;
 }
 
 const addedProduct = computed(() => {
-    return !added.value ? 'Добавить' : 'Добавлено <svg xmlns="http://www.w3.org/2000/svg" width = "24" height = "24" viewBox = "0 0 24 24" fill = "none" stroke = "currentColor" stroke - width="2" stroke - linecap="round" stroke - linejoin="round" class="lucide lucide-check-icon lucide-check" > <path d="M20 6 9 17l-5-5" /> </svg>';
+  return !isAdded.value
+    ? 'Добавить'
+    : 'Добавлено <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 6 9 17l-5-5" /></svg>';
 });
 
+onBeforeUnmount((): void => {
+  if (timer) clearTimeout(timer);
+});
+
+function handleAdd(): void {
+  isAdded.value = true;
+
+  if (timer) clearTimeout(timer);
+
+  timer = setTimeout((): void => {
+    isAdded.value = false;
+    isControlsVisible.value = false;
+  }, 700)
+}
+
+const addButtonClass = computed(() =>
+  isAdded.value
+    ? 'text-white btn btn-success ml-3'
+    : 'btn btn-primary ml-3'
+);
 </script>
 
 <template>
-    <button @click="show = true" v-if="!show" class="btn btn-primary">В корзину</button>
-    <div v-else class="flex">
-        <div v-if="!added" class="flex gap-0.5">
-            <button @click="upCount()" class="btn btn-primary">+</button>
-            <button class="btn btn-primary cursor-default">{{ count }}</button>
-            <button @click="downCount()" class="btn btn-primary">-</button>
-        </div>
-        <button @click="added = true" :class="!added ? 'btn btn-primary ml-3' : 'text-white btn btn-success ml-3'"
-            v-html="addedProduct"></button>
+  <button @click="isControlsVisible = true" v-if="!isControlsVisible" class="btn btn-primary">В корзину</button>
+  <div v-else class="flex">
+    <div v-if="!isAdded" class="flex gap-0.5">
+      <button @click="upCount()" class="btn btn-primary">+</button>
+      <button class="btn btn-primary cursor-default">{{ count }}</button>
+      <button @click="downCount()" class="btn btn-primary">-</button>
     </div>
+    <button @click="handleAdd" :class="addButtonClass">
+      <template v-if="!isAdded">
+        Добавить
+      </template>
+      <template v-else>
+        Добавлено
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+          stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+          class="inline-block ml-1">
+          <path d="M20 6 9 17l-5-5" />
+        </svg>
+      </template>
+    </button>
+  </div>
 </template>
